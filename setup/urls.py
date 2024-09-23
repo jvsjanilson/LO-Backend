a16a5ab7a4d175db20ef8cc926212523e75f6225
Janilson Varela <janilsonjvs@gmail.com>
2024-09-22 21:47:35 -0300

    feat: rota para user autenticado

--- setup/urls.py
+++ setup/urls.py
@@ -20,10 +20,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from core.views import LivroViewSet, FormapagamentoViewSet
+from core.views import LivroViewSet, FormapagamentoViewSet, UserAuthenticadedViewSet
 from compra.views import CompraViewSet
 from rest_framework.routers import DefaultRouter
 from django.http import HttpResponseRedirect
+
 
 router = DefaultRouter()
 router.register("livros", LivroViewSet)
@@ -36,5 +37,6 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include(router.urls)),
+    path("api/user/me/", UserAuthenticadedViewSet.as_view()),
     path("", lambda request: HttpResponseRedirect("api/")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
