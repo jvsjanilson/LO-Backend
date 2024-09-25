4e33b099c04563f50ad8b775384c0df9ba32f24e
Janilson Varela <janilsonjvs@gmail.com>
2024-09-25 07:00:09 -0300

    feat: ajustando view de core

--- core/views.py
+++ core/views.py
@@ -1,47 +1,14 @@
-from core.serializers import (
-    LivroSerializer,
-    FormapagamentoSerializer,
-    RegisterUserSerializer,
-)
-from core.models import Livro, Formapagamento
+from core.serializers import RegisterUserSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import (
-    IsAuthenticatedOrReadOnly,
     IsAuthenticated,
     AllowAny,
 )
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework import mixins
 from rest_framework.views import APIView
 from django.http import JsonResponse
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
-
-
-class LivroViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    queryset = Livro.objects.all()
-    serializer_class = LivroSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [
-        DjangoFilterBackend,
-        SearchFilter,
-    ]
-    search_fields = ["categoria", "titulo", "autor"]
-    filterset_fields = ["categoria", "titulo", "autor"]
-
-
-class FormapagamentoViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    queryset = Formapagamento.objects.all()
-    serializer_class = FormapagamentoSerializer
-    permission_classes = [IsAuthenticated]
-    pagination_class = None
-    filter_backends = [
-        DjangoFilterBackend,
-        SearchFilter,
-        OrderingFilter,
-    ]
 
 
 class UserAuthenticadedViewSet(APIView):
