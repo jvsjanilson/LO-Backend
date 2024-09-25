cb0d384b5907fcccc9257a4dff5f54ab8d0bc744
Janilson Varela <janilsonjvs@gmail.com>
2024-09-25 09:51:02 -0300

    feat: ajustado serializer para ler e criar item da compra

--- compra/serializers.py
+++ compra/serializers.py
@@ -21,24 +21,25 @@ class ItemSerializer(ModelSerializer):
 
 class CompraSerializer(ModelSerializer):
     items = serializers.SerializerMethodField()
-    # items = ItemSerializer(many=True, read_only=True)
+    
+    items_create = ItemSerializer(many=True, write_only=True)
 
     class Meta:
         model = Compra
-        fields = ["id", "user",  "created_at", "items"]
+        fields = ["id", "user",  "created_at", "items", "items_create"]
 
     def validate(self, attrs):
 
         if attrs["user"] != self.context["request"].user:
             raise serializers.ValidationError({"user": "Usuário inválido."})
 
-        if not attrs["items"]:
+        if not attrs["items_create"]:
             raise serializers.ValidationError({"items": "Compra sem itens."})
 
         return super().validate(attrs)
 
     def create(self, validated_data):
-        items_data = validated_data.pop("items")
+        items_data = validated_data.pop("items_create")
         compra = Compra.objects.create(**validated_data)
         for item_data in items_data:
             Item.objects.create(compra=compra, **item_data)
